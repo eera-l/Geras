df9c20fbd159604028249a857ff11d8d8704febc
eera-l <federica.comuni@gmail.com>
2019-04-11 15:21:43 +0200

    Added dementia features

--- Software/texW2V/splitter.py
+++ Software/texW2V/splitter.py
@@ -3,23 +3,35 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = '/home/federica/Documents/Thesis/Programs/TextParser/control/control_features/'
+pathControl = '/home/federica/Documents/Thesis/Programs/TextParser/control/control_features/'
+pathDementia = '/home/federica/Documents/Thesis/Programs/TextParser/dementia/dementia_features'
 
-files = []
+filesC = []
+filesD = []
 rows = []
 
-for r, d, f in os.walk(path):
+for r, d, f in os.walk(pathControl):
     for file in f:
         if '.cha' in file:
-            files.append(os.path.join(r, file))
+            filesC.append(os.path.join(r, file))
+
+for r, d, f in os.walk(pathDementia):
+    for file in f:
+        if '.cha' in file:
+            filesD.append(os.path.join(r, file))
 
 
-for file in files:
+for file in filesC:
     file = open(file, "r")
     lines = file.read()
     words = lines.split("\n")[:-1]
     rows.append(words)
-    print(rows)
+
+for file in filesD:
+    file = open(file, "r")
+    lines = file.read()
+    words = lines.split("\n")[:-1]
+    rows.append(words)
 
 df = pd.DataFrame(np.array(rows), columns=['utterances', 'predicates', 'sentences', 'unique_words', 'utterances_length',
                                            'subordinates', 'coordinates',
@@ -28,7 +40,8 @@ df = pd.DataFrame(np.array(rows), columns=['utterances', 'predicates', 'sentence
                                            'pauses', 'trailing_offs', 'overlaps', 'repetitions', 'retracing_correction',
                                            'retracing_reform', 'type_token_ratio', 'adverbs_ratio',
                                            'adjectives_ratio', 'idea_density'])
-#df['hesitations'].hist()
-#plt.show()
+df['utterances'].hist()
+plt.show()
+#df.to_csv('dataset', index=False)
 print(df)
 
