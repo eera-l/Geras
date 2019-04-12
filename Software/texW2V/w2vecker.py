d3b9ee9eef483dc2aaac107050bd0289f5fd9b83
eera-l <federica.comuni@gmail.com>
2019-04-12 16:01:39 +0200

    Added csv and completed word2vec distance

--- Software/texW2V/w2vecker.py
+++ Software/texW2V/w2vecker.py
@@ -1,12 +1,44 @@
 import gensim
 import os
 from random import randint
+from gensim.models.word2vec import Word2Vec
+import gensim.downloader as api
 
-model = gensim.models.KeyedVectors.load_word2vec_format('/home/federica/Documents/Thesis/Datasets/GoogleNews-vectors-negative300.bin', binary=True)
-print(model.distance('man', 'woman'))
+#model = gensim.models.KeyedVectors.load_word2vec_format('/home/federica/Documents/Thesis/Datasets/GoogleNews-vectors-negative300.bin', binary=True)
+top_directory = "/home/federica/Documents/Thesis/Programs/TextParser/corpus.txt"
+
+#corpus = api.load("/home/federica/Documents/Thesis/Programs/TextParser/corpus.txt")
+
+
+
+# def iter_documents(top_directory):
+#     """Iterate over all documents, yielding a document (=list of utf8 tokens) at a time."""
+#     document = open(top_directory).read()  # read the entire document, as one big string
+#     document = document.replace("\n", " ")
+#     yield gensim.utils.tokenize(document, lower=True) # or whatever tokenization suits you
+#
+#
+# class MyCorpus(object):
+#     def __init__(self, top_dir):
+#         self.top_dir = top_dir
+#         self.dictionary = gensim.corpora.Dictionary(iter_documents(top_dir))
+#         self.dictionary.filter_extremes(no_below=1, keep_n=30000) # check API docs for pruning params
+#
+#     def __iter__(self):
+#         for tokens in iter_documents(self.top_dir):
+#             yield self.dictionary.doc2bow(tokens)
+#
+#
+# corpus = MyCorpus(top_directory)  # create a dictionary
+# for vector in corpus:  # convert each document to a bag-of-word vector
+#     print(vector)
+
+model = api.load('fasttext-wiki-news-subwords-300')
+#model = Word2Vec(corpus)
+print(model.most_similar("cat"))
 
 path = '/home/federica/Documents/Thesis/Programs/TextParser/dementia/dementia_no_functional_clean/'
-pathControl = '/home/federica/Documents/Thesis/control/control_description'
+pathControl = '/home/federica/Documents/Thesis/control_description'
 
 files = []
 
@@ -19,7 +51,7 @@ words = []
 lines = []
 
 distances = []
-total = []
+total = dict()
 randoms = []
 
 randomDistances = []
@@ -30,8 +62,8 @@ data = contentsControl.split(" ")
 data[len(data) - 1] = "hedge"
 
 for file in files:
-    file = open(file, "r")
-    contents = file.read()
+    with open(file, "r") as f:
+        contents = f.read()
     dataFile = contents.split("\n")
 
     for df in dataFile:
@@ -46,28 +78,37 @@ for file in files:
 
     for w in words:
         for ww in data:
-            distances.append(model.similarity(w, ww))
+            try:
+                distances.append(model.similarity(w, ww))
+            except KeyError as e:
+                pass
 
     words.clear()
-    for i in range(0, 50):
-        num = randint(0, distances(total) - 1)
-        while num in randoms:
-            num = randint(0, distances(total) - 1)
-        randoms.append(num)
 
-    #for i in randoms:
-        #something
+
+
 
     average = sum(distances) / len(distances)
     print(average)
     distances.clear()
-    total.append(average)
+    total[os.path.basename(file)] = average
 
-for i in range(0, 50):
-    randomDistances.append(total[randoms[i]])
+for file, average in total.items():
+    with open("/home/federica/Documents/Thesis/Programs/TextParser/dementia/dementia_w2v_distance/" + file,
+                "w") as singlefile:
+        singlefile.write(str(average))
+
+# for i in range(0, 50):
+    #     num = randint(0, distances(total) - 1)
+    #     while num in randoms:
+    #         num = randint(0, distances(total) - 1)
+    #     randoms.append(num)
+
+# for i in range(0, 50):
+#     randomDistances.append(total[randoms[i]])
 
 print("============================")
-print(sum(randomDistances) / len(randomDistances))
+print(sum(total.values()) / len(total))
 
 
 
