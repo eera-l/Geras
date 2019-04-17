551cd21c7cb8824ef4678f184ba8d284a611560e
eera-l <federica.comuni@gmail.com>
2019-04-17 16:05:21 +0200

    Plotted data, added normalization

--- Software/texW2V/scikiter.py
+++ Software/texW2V/scikiter.py
@@ -1,7 +1,9 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
-import numpy as np
+
+import matplotlib.pyplot as plt
 import pandas as pd
 
 #Read data
@@ -12,7 +14,6 @@ df = shuffle(df)
 y = df['dementia'].apply(lambda x : 1 if x == 'Y' else 0)
 x = df.drop(columns=['dementia'])
 
-#TODO split x and y into 80-20
 split_rate = 0.2
 split_index = int(split_rate * df.shape[0])
 x_val = x[:split_index]
@@ -21,22 +22,29 @@ y_val = y[:split_index]
 x_train = x[split_index:]
 y_train = y[split_index:]
 
+#scaler = StandardScaler()
+#x_train = scaler.fit_transform(x_train)
+#x_val = scaler.transform(x_val)
 
-
-
-#valid = pd.read_csv('validation_set', sep=',')
 
 #training of the model
-model = BernoulliNB()
+model = GaussianNB()
 model.fit(x_train, y_train)
 
 #evaluation
 score = model.score(x_train, y_train)
-
-#predicted = model.predict(valid)
 
 print(score)
 
 score = model.score(x_val, y_val)
 
 print(score)
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# cax = plt.matshow(x.corr())
+# ax.set_xticklabels([''])
+# ax.set_yticklabels([''])
+#
+# fig.colorbar(cax)
+# plt.show()
