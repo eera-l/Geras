e07c4ab69239165189c0b9288fd5be9dd1b475c9
eera-l <federica.comuni@gmail.com>
2019-04-25 14:15:46 +0200

    Created artificial neural network

--- Software/texW2V/ann.py
+++ Software/texW2V/ann.py
@@ -1,9 +1,37 @@
+import numpy
+import pandas as pd
 from keras import Sequential
 from keras.callbacks import EarlyStopping
-from keras.layers import Dense, Dropout
+from keras.layers import Embedding, LSTM, Dense, regularizers, Dropout
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 
-#create model
+df = pd.read_csv('train_set', sep=',')
+
+#Process data
+df = shuffle(df)
+y = df['dementia'].apply(lambda x : 1 if x == 'Y' else 0)
+x = df.drop(columns=['dementia'])
+
+numpy.random.seed(7)
+
+split_rate = 0.2
+split_index = int(split_rate * df.shape[0])
+# x_val = x[:split_index]
+# y_val = y[:split_index]
+#
+# x_train = x[split_index:]
+# y_train = y[split_index:]
+x_train = x
+y_train = y
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+# x_val = scaler.transform(x_val)
+
+
+
+# create model
 model = Sequential()
 model.add(Dense(24, input_dim=24, activation='relu'))
 model.add(Dropout(0.2))
@@ -16,8 +44,8 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 
-es = EarlyStopping(monitor='val_loss', patience=40)
-history = model.fit(x_train, y_train, validation_split=0.2, epochs=1000, batch_size=10,  callbacks=[es])
+es = EarlyStopping(monitor='val_loss', patience=20)
+history = model.fit(x_train, y_train, validation_split=0.2, epochs=1000, callbacks=[es])
 
 
 # evaluate the model
@@ -44,3 +72,4 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+
